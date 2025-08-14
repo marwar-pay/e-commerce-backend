c804a0a7439938321e52cfdcfd6de0ebab476d7c
pankaj chouhan <pankajmarwarpay@gmail.com>
2025-08-14 11:56:18 +0530

    Refactor banner controller to improve create and getBanner methods

--- src/controller/banner.controller.js
+++ src/controller/banner.controller.js
@@ -18,7 +18,12 @@ export const createBanner = async (req, res) => {
       return res.status(500).json({ message: "Error uploading image" });
     }
 
-    const newBanner = new Banner({ imageUrl: bannerImage.url, altText, page, referenceWebsite });
+    const newBanner = new Banner({
+      imageUrl: bannerImage.url,
+      altText,
+      page,
+      referenceWebsite,
+    });
     await newBanner.save();
     res.status(201).json(newBanner);
   } catch (error) {
@@ -28,11 +33,11 @@ export const createBanner = async (req, res) => {
 
 export const getBanner = async (req, res) => {
   try {
-    const banner = await Banner.findOne({ page: req.params?.page });
-    if (!banner) {
+    const banners = await Banner.find({ page: req.params?.page });
+    if (!banners || banners.length === 0) {
       return res.status(404).json({ message: "Banner not found" });
     }
-    res.status(200).json(banner);
+    res.status(200).json(banners);
   } catch (error) {
     res.status(500).json({ message: "Error fetching banners", error });
   }
@@ -41,19 +46,26 @@ export const getBanner = async (req, res) => {
 export const updateBanner = async (req, res) => {
   try {
     const { id } = req.params;
-    const { altText, page } = req.body;
-    if (!id || !altText || !page) {
-      return res.status(400).json({ message: "all fields are required" });
+    if (!id) {
+      return res.status(400).json({ message: "Banner ID is required" });
     }
-    const bannerImagePath = req.file?.path;
-    const bannerImage = await uploadToCloudinary(bannerImagePath);
-    if (!bannerImage) {
-      return res.status(500).json({ message: "Error uploading image" });
+    const { altText, page } = req.body;
+    let updateData = { altText, page };
+
+    if (req.file) {
+      const bannerImagePath = req.file.path;
+      const bannerImage = await uploadToCloudinary(bannerImagePath);
+
+      if (!bannerImage) {
+        return res.status(500).json({ message: "Error uploading image" });
+      }
+
+      updateData.imageUrl = bannerImage.url;
     }
 
     const updatedBanner = await Banner.findByIdAndUpdate(
       id,
-      { altText, imageUrl: bannerImage.url, page },
+      { ...updateData },
       { new: true }
     );
     if (!updatedBanner) {
